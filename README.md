7d7bab56d5d97384074497773bcf8a8ed80ef1b8
Rommel <rltb-rommel.github.io>
2024-09-22 09:50:38 +0800

    Prep01

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # cse210-ww-student-template
-This is the starter code for students in the worldwide content version of CSE 210.
+
+Rommel F. Aunario
+
+Homework Platform CSE 210.
 
 This repository contains the starter code for many different projects. They are arranged as follows:
 
--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine();  // added for in between space        
+        // prompt for the user's first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        
+        // prompt for the user's last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine(); //added for in between space
+        // display combined results
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine();  // added for bottom space     
+        
     }
 }
+
