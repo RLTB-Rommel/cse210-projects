b212935f4c383cd396d29fae1e953b070a2e492c
Rommel <rltb-rommel.github.io>
2024-09-22 08:26:13 +0800

    Changed message

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Yahoo!");
+        Console.WriteLine("Hello World!Amazing Work");
+        Console.WriteLine("Yahoo!I've done it!");
     }
 }
