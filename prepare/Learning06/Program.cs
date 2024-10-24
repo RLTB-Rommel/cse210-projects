7c448c54ef168f8b2a6443e6727a2d7981c73435
RLTB-Rommel <rommelaunario101@gmail.com>
2024-10-25 05:40:05 +0800

    w06 shapes

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: { shape.GetColor()}, Area: { shape.GetArea()}");
+        }
     }
 }
